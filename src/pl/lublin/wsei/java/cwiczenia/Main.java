61dcaa1b65c3edd44a47c8e5b57c45fada349bea
Dawid <dawiddrazyk99@o2.pl>
2021-03-21 17:14:36 +0100

    Działa

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,14 +1,15 @@
-//Zadanie 7d + e
+//Zadanie 7g
 package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        int a = 3;
-        double b = 4.21;
-        String s = "jakiś tekst";
+        System.out.println("Nazywaliśmy to \"Witaminą B3\"");
 
-        System.out.printf("a = %d, b = %.2f, s = %20s %n",a,b,s);
+        System.out.printf("alfa\tsin(alfa)\n");
+        for (int i = 0; i < 370; i+= 10)
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI
 
     }
 }
