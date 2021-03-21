365d46b0505c0455ebbfd56604d3b78f160f1f62
Dawid <dawiddrazyk99@o2.pl>
2021-03-21 17:02:13 +0100

    Działa

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,16 +1,11 @@
-//Zadanie 4a
 package pl.lublin.wsei.java.cwiczenia;
 
 /** java doct */
 public class Main {
 
-    public static void main(String[] args)  {
-        System.out.println ("Hello Java world\n");
+    public static void main(String[] args) {
+        System.out.print("Ala\n");
+        System.out.print("ma\n");
+        System.out.print("kota\n");
     }
-    // Dzien dobry
-        /*
-        komentarz wielo
-        linijkowy
-
-         */
 }
