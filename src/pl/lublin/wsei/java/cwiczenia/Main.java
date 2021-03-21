0822303f4f8313fb02e5f4b4ede49a52bc736fe9
Dawid <dawiddrazyk99@o2.pl>
2021-03-21 17:16:25 +0100

    Działa

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,4 +1,4 @@
-//Zadanie 9b
+//Zadanie 9c
 package pl.lublin.wsei.java.cwiczenia;
 
 import java.util.Scanner;
@@ -8,8 +8,9 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int num1=0, num2=0;
+
         do {
+            int num1=0, num2=0;
             System.out.print("Podaj pierwszą liczbę");
             num1 = input.nextInt();
             if (num1 != 0) {
@@ -17,6 +18,5 @@ public class Main {
                 num2 = input.nextInt();
             }
             System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
-        } while ((num1 != 0) && (num2 != 0));
+        } while ((num1 != 0) && (num2 != 0)); //program nie dziala
     }
-}
