290edc014dee864aeafc09068368af81338be179
Dawid <dawiddrazyk99@o2.pl>
2021-06-01 17:16:09 +0200

    Lab_2e

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,4 +1,13 @@
-public static boolean isPalindrome(String word){
-        for(int i = 0; i < word.length() / 2; i++){
-        if(word.charAt(i) != word.charAt(word.length() - 1 - i))
-        return false;
+public  static String shuffle (String word){
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+
+        for(int i = word.length() - 1; i >= 0; i--){
+        int index = random.nextInt(i);
+
+        builder.append(word.charAt(index));
+        word = word.substring(0, index) + word.substring(index + 1);
+        }
+
+        return builder.toString();
+        }
