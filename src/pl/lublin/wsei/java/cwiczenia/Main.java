8321c31480fb4c0c5bafd95a6a6fb5a677629a80
Dawid <dawiddrazyk99@o2.pl>
2021-06-01 17:15:23 +0200

    Lab_2c

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,15 +1,12 @@
-public  static  String camelize(String word){
+public  static  String decamelize(String word){
         StringBuilder builder = new StringBuilder();
 
-        String[] words = word.split(" ");
-        builder.append(words[0].toLowerCase());
+        for(int i = 0; i < word.length(); i++){
+        if(Character.isUpperCase(word.charAt(i)){
+        builder.append(" ");
+        }
 
-        for(int i = 1; i < words.length; i++){
-        String stringToAdd = words[i].substring(1).toLowerCase();
-
-        builder.append(Character.toUpperCase(words[i].charAt(0)));
-        builder.append(stringToAdd);
+        builder.append(word.charAt(i));
         }
 
         return builder.toString();
-        }
